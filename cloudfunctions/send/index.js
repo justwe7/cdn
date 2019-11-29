b3486b8bd69654fdf24905a65ffc6d697fb22f83
justwe7 <x13133053566@163.com>
2019-11-29 14:12:15 +0800

    feat: 云提醒testcode

--- cloudfunctions/send/index.js
+++ cloudfunctions/send/index.js
@@ -4,17 +4,49 @@ exports.main = async (event, context) => {
   cloud.init();
   const db = cloud.database();
   const _ = db.command
-  console.log('调用通知');
+  const $ = db.command.aggregate
+  console.log('--通知调用--');
   const Timestamp = +new Date()
+  /* const preMin = (Timestamp - 600000)
+  const nextMin = (Timestamp + 600000) */
+  // console.log(Timestamp, preMin);
+  
+  // var d = new Date('2019-10-01'), d2 = new Date('2019-11-01 12:00:00')
+  // var preMin = new Date(Timestamp - 600000), nextMin = new Date(Timestamp + 600000)
+  /* var preMin = new Date('2019-11-29'), nextMin = new Date('2019-11-30')
+  var a = $.dateFromString({
+      dateString: preMin.toJSON()
+  }), a2 = $.dateFromString({
+      dateString: nextMin.toJSON()
+  })
+  console.log(new Date(db.serverDate()).toLocaleString()); */
+  
+
+
   try {
     // 从云开数据库中查询等待发送的消息列表
+    /* const messages = await db.collection('demo').aggregate()
+    .match({
+      done: false,
+      // notice: true
+    })
+    .addFields({
+        matched: $.and([$.gte(['$remindDate', a]), $.lte(['$remindDate', a2])]),
+    })
+    .match({
+        matched: true
+    })
+    .end();
+    console.log(messages); */
+    
     const messages = await db
       .collection('demo')
       // 查询条件这里做了简化，只查找了状态为未发送的消息
       // 在真正的生产环境，可以根据开课日期等条件筛选应该发送哪些消息
       .where({
         done: false,
-        remindDate: _.gte(Timestamp)
+        notice: true,
+        // remindDate: _.gte(preMin).and(_.lte(nextMin))
       })
       .get();
 
@@ -27,16 +59,16 @@ exports.main = async (event, context) => {
           touser: res.touser,
           page: res.page,
           data: res.message,
-          templateId: 'lpT7pYMb_irS75lxytdtb1JbhyTR75noRtbFST-DZwk',
-          // templateId: res.templateId,
+          // templateId: 'lpT7pYMb_irS75lxytdtb1JbhyTR75noRtbFST-DZwk',
+          templateId: res.templateId,
         });
-        // 发送成功后将消息的状态改为已发送
+        // 发送成功后将消息的通知关闭
         return db
           .collection('demo')
           .doc(res._id)
           .update({
             data: {
-              done: true,
+              notice: false,
             },
           });
       } catch (e) {
--- src/pages/taskList/taskList.js
+++ src/pages/taskList/taskList.js
@@ -1,12 +1,60 @@
-const app = getApp()
-const db = wx.cloud.database()
-const _ = db.command
+//index.js
+const app = getApp();
+const lessonTmplId = 'GebGfrYLwpxDDGDUmQcU7X3aQv-uomNvHF2jhjg26HQ';
 
 Page({
   data: {
-    taskDetail: {}
+    lessons: [
+      {
+        id: 1,
+        thing4: {value: '云开发接入订阅消息课程'},
+        date3: {value: '2019-10-22'},
+        thing2: {value: '教你用云开发快速接入订阅消息'},
+      },
+      {
+        id: 2,
+        thing4: {value: '云开发定时触发器学习课程'},
+        date3: {value: '2019-10-22'},
+        thing2: {value: '教你使用定时触发器'},
+      },
+    ],
   },
-  onShow(option) {},
-  onLoad(option) {
-  }
-})
+  onSubscribe: function(e) {
+    // 获取课程信息
+    const item = e.currentTarget.dataset.item;
+
+    // 调用微信 API 申请发送订阅消息
+    wx.requestSubscribeMessage({
+      // 传入订阅消息的模板id，模板 id 可在小程序管理后台申请
+      tmplIds: [lessonTmplId],
+      success(res) {
+        // 申请订阅成功
+        if (res.errMsg === 'requestSubscribeMessage:ok') {
+          // 这里将订阅的课程信息调用云函数存入db
+          wx.cloud
+            .callFunction({
+              name: 'subscribe2',
+              data: {
+                data: item,
+                templateId: lessonTmplId,
+              },
+            })
+            .then(() => {
+              wx.showToast({
+                title: '订阅成功',
+                icon: 'success',
+                duration: 2000,
+              });
+            })
+            .catch(() => {
+              wx.showToast({
+                title: '订阅失败',
+                icon: 'success',
+                duration: 2000,
+              });
+            });
+        }
+      },
+    });
+  },
+});
